0090e0dc62a7e6243983ef4266308a578608069f
wwfgxnyi <47392135+wwfgxnyi@users.noreply.github.com>
2023-06-03 01:07:39 +0200

    Update hello.py

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -33,7 +33,7 @@ class Hello(Plugin):
         if e_context["context"].type == ContextType.JOIN_GROUP:
             e_context["context"].type = ContextType.TEXT
             msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f'请你随机使用一种风格说一句问候语来欢迎新用户"{msg.actual_user_nickname}"加入群聊。'
+            e_context["context"].content = f'请你随机使用一种风格说一句简体中文普通话的问候语来欢迎新用户"{msg.actual_user_nickname}"加入群聊。'
             e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
             return
 
